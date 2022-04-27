4672976837d613452f4bc9c57fc1c7d586c2910f
taipansec <y.b@taipan.systems>
2022-04-27 12:01:30 +0400

    Moving to python

--- lipol.sh
+++ lipol.sh
@@ -184,7 +184,7 @@ function fscheck() {
 
     echotitle "1.1.22 Ensure sticky bit is set on all world-writable directories"
     df=( $(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type d \( -perm -0002 -a ! -perm -1000 \) 2>/dev/null) )
-    echo $df
+    echo "MOVING TO PYTHON SCRIPT..."
 }
 
 fscheck
